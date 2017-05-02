b56360429b537d1a5d5184cc927814f49f2eeac8
Chris Jones <gwyn20@gmail.com>
2017-05-01 21:52:35 -0400

    Checkpoint 10 Directives Part 2: Completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -19,7 +19,9 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
+            scope: {
+                onChange: '&'
+            },
             link: function(scope, element, attributes) {
                 scope.value = 0;
                 scope.max = 100;
@@ -29,6 +31,14 @@
                 * @type {Object}
                 */
                 var seekBar = $(element);
+                
+                attributes.$observe('value', function(newValue) {
+                scope.value = newValue;
+                });
+ 
+                attributes.$observe('max', function(newValue) {
+                scope.max = newValue;
+                });
  
                 /**
                 * @function percentString
@@ -69,6 +79,7 @@
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
                 
                 /**
@@ -81,6 +92,7 @@
                         var percent = calculatePercent(seekBar, event);
                         scope.$apply(function() {
                             scope.value = percent * scope.max;
+                            notifyOnChange(scope.value);
                         });
                     });
  
@@ -88,6 +100,12 @@
                         $document.unbind('mousemove.thumb');
                         $document.unbind('mouseup.thumb');
                     });
+                };
+                
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
                 };
             }
         };
